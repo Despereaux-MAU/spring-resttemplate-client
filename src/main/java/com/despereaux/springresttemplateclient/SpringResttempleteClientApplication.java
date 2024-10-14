7d64798a18697f9853c60798f900ef72b3e2c3b3
Despereaux <rlaehdwn025@naver.com>
2024-10-14 11:25:55 +0900

    init commit

---
+++ src/main/java/com/despereaux/springresttemplateclient/SpringResttempleteClientApplication.java
@@ -0,0 +1,13 @@
+package com.despereaux.springresttemplateclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResttempleteClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResttempleteClientApplication.class, args);
+    }
+
+}
